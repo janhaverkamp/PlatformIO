ed55521f51ecfa3fa454f0c004dc2cd4a999a16a
Jan Haverkamp <jr.haverkamp@gmail.com>
2018-04-09 23:46:34 +0200

    update

--- Projects/ShiftRegister-test-0.1-09.04.18/src/main.cpp
+++ Projects/ShiftRegister-test-0.1-09.04.18/src/main.cpp
@@ -1,28 +1,41 @@
 #include <Arduino.h>
-using namespace std;
+//using namespace std;
 
 #define DATA    4
 #define CLOCK   2
 #define LATCH   3
 #define REGISTERS   3
-#define BITS        0,0,0,0,0,0,0,0, \
-                    0,0,0,0,0,0,0,0, \
-                    0,0,1,0,0,0,0,0
+#define BITS        0,0,0,0,0,0,0,0,\
+                    0,0,0,0,0,0,0,0,\
+                    0,0,0,0,0,1,0,1
 
-void registerWrite(bool bits[])
+/*void registerWrite(bool arr[])
 {
     digitalWrite(LATCH,0);
-    for(int i(REGISTERS * 8); i>=0; i--)
+    for(int k(REGISTERS * 8); k>=0; k--)
     {
         digitalWrite(CLOCK,0);
-        digitalWrite(DATA,bits[i]);
+        digitalWrite(DATA,arr[k]);
         digitalWrite(CLOCK,1);
     }
     digitalWrite(LATCH,1);
 }
+*/
+
+void registerWrite(byte value)
+{
+    int p(0);
+    bool bits[REGISTERS*8];
+
+    for(int ref; ref < value; ref = pow(2,(p+2)), p++);
+    Serial.println(p);
+    return bits;
+}
 
 void setup()
 {
+    Serial.begin(9600);
+
     pinMode(DATA,OUTPUT);
     pinMode(CLOCK,OUTPUT);
     pinMode(LATCH,OUTPUT);
@@ -30,7 +43,6 @@ void setup()
 
 void loop()
 {
-    bool arr[] = {BITS};
-    registerWrite(arr);
+    registerWrite(10);
 }
 
