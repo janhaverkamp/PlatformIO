b4e606018b5fcd37ee55afe816daac1961a47b38
Jan Haverkamp <jr.haverkamp@gmail.com>
2018-04-10 01:14:46 +0200

    01:14

--- Projects/ShiftRegister-test-0.1-09.04.18/src/main.cpp
+++ Projects/ShiftRegister-test-0.1-09.04.18/src/main.cpp
@@ -1,11 +1,10 @@
 #include <Arduino.h>
-//using namespace std;
 
 #define DATA    4
 #define CLOCK   2
 #define LATCH   3
 #define REGISTERS   3
-#define BITS        0,0,0,0,0,0,0,0,\
+//#define BITS        0,0,0,0,0,0,0,0,\
                     0,0,0,0,0,0,0,0,\
                     0,0,0,0,0,1,0,1
 
@@ -24,12 +23,26 @@
 
 void registerWrite(byte value)
 {
-    int p(0);
-    bool bits[REGISTERS*8];
+    int p(REGISTERS*8);
+    //bool bits[REGISTERS*8];
 
-    for(int ref; ref < value; ref = pow(2,(p+2)), p++);
-    Serial.println(p);
-    return bits;
+    digitalWrite(LATCH,0);
+
+    for(; p >= 0; p--)
+    {
+        digitalWrite(CLOCK,0);
+        if(value >= pow(2,p))
+        {
+            value = value - pow(2,p);
+            digitalWrite(DATA,1); //bits[p] = 1;
+        }
+        else digitalWrite(DATA,0); //bits[p] = 0;
+
+        digitalWrite(CLOCK,1);
+        digitalWrite(CLOCK,0);
+    }
+    digitalWrite(LATCH,1);
+
 }
 
 void setup()
@@ -39,10 +52,21 @@ void setup()
     pinMode(DATA,OUTPUT);
     pinMode(CLOCK,OUTPUT);
     pinMode(LATCH,OUTPUT);
+
+
 }
+
+
 
 void loop()
 {
-    registerWrite(10);
+    registerWrite(0x38);
+    delay(1000);
+    registerWrite(0x06);
+    delay(1000);
+    registerWrite(0x6D);
+    delay(1000);
+    registerWrite(0x77);
+    delay(1000);
 }
 
