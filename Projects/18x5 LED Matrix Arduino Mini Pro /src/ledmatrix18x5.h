064c38fe3c2f9fe39dd5da01e1211cadf4e75e00
Jan Haverkamp <jr.haverkamp@gmail.com>
2018-04-17 02:48:51 +0200

    Cleaner version 0.0.3

--- Projects/18x5 LED Matrix Arduino Mini Pro /src/ledmatrix18x5.h
+++ Projects/18x5 LED Matrix Arduino Mini Pro /src/ledmatrix18x5.h
@@ -1,20 +1,24 @@
-#ifndef LEDMATRIX18X15
+#ifndef LEDMATRIX18X15                          //Definition of headerfile
 
-#define DATA    10
+#define DATA    10                              //Shiftregister pins
 #define CLOCK   12
 #define LATCH   11
 #define REGISTERS   2
+                                                //Macros for shiftregister-pulses
+#define CLOCKPULSE digitalWrite(CLOCK,0x01);\
+                   digitalWrite(CLOCK,0x00);
+#define LATCHPULSE digitalWrite(LATCH,0x01);\
+                   digitalWrite(LATCH,0x00);
 
-#define CLOCKPULSE  digitalWrite(CLOCK,0x01); digitalWrite(CLOCK,0x00);
-#define LATCHPULSE   digitalWrite(LATCH,0x01); digitalWrite(LATCH,0x00);
-
-#define ROWS    5
+#define ROWS    5                               //Definition of the Led-Matrix
 #define COLUMNS 18
 
-const int yarray[] = {4,6,7,8,9};
-int dpadinput;
+const int yarray[] = {4,6,7,8,9};               //Pinout-array for loops and etc..
+int dpadinput;                                  //Input var for D-Pad interrupts
 
-static bool arr[5][18] ={
+                                                //Display arrays 
+
+static bool arr[5][18] ={                       //Clear global array
     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
@@ -22,7 +26,7 @@ static bool arr[5][18] ={
     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
 };
 
-bool png[5][18] ={
+const bool png[5][18] ={                        //PONG bitmap array
     {0,0,0,1,1,0,0,1,0,0,0,1,0,0,1,1,1,1},
     {0,0,1,0,0,1,0,1,1,0,0,1,0,1,0,0,0,0},
     {0,0,1,1,1,0,0,1,0,1,0,1,0,1,1,1,1,0},
@@ -30,7 +34,7 @@ bool png[5][18] ={
     {0,0,1,0,0,0,0,1,0,0,0,1,0,0,1,1,1,0}
 };
 
-bool tst[5][18] ={
+const bool tst[5][18] ={                        //TEST bitmap array
     {0,0,1,1,1,0,1,1,1,0,0,1,1,1,0,1,1,1},
     {0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0,1,0},
     {0,0,0,1,0,0,1,1,0,0,0,1,1,0,0,0,1,0},
@@ -38,7 +42,7 @@ bool tst[5][18] ={
     {0,0,0,1,0,0,1,1,1,0,1,1,1,0,0,0,1,0}
 };
 
-bool hme[5][18] ={
+const bool hme[5][18] ={                        //HOME bitmap array
     {0,0,0,1,1,1,0,0,0,1,1,1,0,0,1,1,1,0},
     {0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,1},
     {0,0,1,0,0,0,1,0,1,0,0,0,1,0,1,1,1,0},
@@ -46,17 +50,17 @@ bool hme[5][18] ={
     {0,0,0,1,1,1,0,0,0,1,1,1,0,0,1,1,1,0}
 };
 
-void initShifts()//SHIFT REGISTER
+void initShifts()//SHIFT REGISTER               //Initialisation of shiftregisters
 {
-    pinMode(DATA,OUTPUT);
+    pinMode(DATA,OUTPUT);                       //Setting the outputs
     pinMode(CLOCK,OUTPUT);
     pinMode(LATCH,OUTPUT);
-    pinMode(A3,OUTPUT);     //Output for two last columns
+    pinMode(A3,OUTPUT);                         //Output for two last columns, that aren't in the registers
     pinMode(A2,OUTPUT);
-    for(int i(4);i<=9; i++) pinMode(i,OUTPUT);      //Output of Rows
+    for(int i(4);i<=9; i++) pinMode(i,OUTPUT);  //Output for the Rows
 }
 
-void pciSetup(byte pin)
+void pciSetup(byte pin)                         //Setup of general interrupt-funtion
 {
     *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));
     PCIFR |= bit(digitalPinToPCICRbit(pin));
@@ -64,61 +68,60 @@ void pciSetup(byte pin)
 
 }
 
-void initDpad()		//Init DPAD
+void initDpad()		                            //Initialisation of the DPAD
 {
-	pinMode(A1,INPUT);      //DPAD analog in
-    pinMode(A0,INPUT);      //DPAD analog in
-    pinMode(A5,INPUT);      //DPAD analog in
-    pinMode(A4,INPUT);      //DPAD analog in
+	pinMode(A1,INPUT);                          //DPAD analog in
+    pinMode(A0,INPUT);
+    pinMode(A5,INPUT);
+    pinMode(A4,INPUT);
 
-    digitalWrite(A0, HIGH); //Internal pullups
+    digitalWrite(A0, HIGH);                     //Internal pullups
     digitalWrite(A1, HIGH);
     digitalWrite(A4, HIGH);
     digitalWrite(A5, HIGH);
 
-    pciSetup(A0);
+    pciSetup(A0);                               //Setup interrupt-pins
     pciSetup(A1);
     pciSetup(A4);
     pciSetup(A5);
 }
 
-ISR (PCINT1_vect)
+ISR (PCINT1_vect)                               //Interupt-routine function
 {
-    if(digitalRead(A0) == 1) dpadinput = 1;
+    if(digitalRead(A0) == 1) dpadinput = 1;     //Write values into var, when interrupting
     if(digitalRead(A1) == 1) dpadinput = 4;
     if(digitalRead(A4) == 1) dpadinput = 2;
     if(digitalRead(A5) == 1) dpadinput = 3;
 
 }
 
-void matrixDot(int x, int y)
+void matrixDot(int x, int y)                    //Old function that displays only one pixel, by (x)(y)
 {
     for(int pin(4); pin <= 9; pin++) digitalWrite(pin,0x01);    //Turn Y pins off as default
-    digitalWrite(A2,0x00);
+    digitalWrite(A2,0x00);                      //Turn of two X over pins 
     digitalWrite(A3,0x00);
 
-    if     (y==1) digitalWrite(4,0x00);
+    if     (y==1) digitalWrite(4,0x00);         //Decide which row to turn on, by Y
     else if(y==2) digitalWrite(6,0x00);
     else if(y==3) digitalWrite(7,0x00);
     else if(y==4) digitalWrite(8,0x00);
     else if(y==5) digitalWrite(9,0x00);
 
-    if(x == 1) digitalWrite(A3,0x01);
+    if(x == 1) digitalWrite(A3,0x01);           //Turn on A3 / A2 when not in registers
     if(x == 2) digitalWrite(A2,0x01); 
-    if(x >= 3)
+    if(x >= 3)                                  //If in area of registers. Display function
         {
-        digitalWrite(LATCH,0);
+        digitalWrite(LATCH,0);                  //Clock and Latch off-pulses
         digitalWrite(CLOCK,0);
 
-        for(int a(18); a >= 0; a--) 
+        for(int a(18); a >= 0; a--)             //Loop for the shiftregisters to get data and clock pulses
             {
-                if(a == -(x-18)) digitalWrite(DATA,0x01); 
-            digitalWrite(CLOCK,0x01);
-            digitalWrite(DATA,0x00);
-            digitalWrite(CLOCK,0x00);
+                if(a == -(x-18)) digitalWrite(DATA,0x01);   //Pulse DATA HIGH, when true
+            digitalWrite(CLOCK,0x01);           //Clock pulse
+            digitalWrite(DATA,0x00);            //Data off
+            digitalWrite(CLOCK,0x00);           //Clock pulse off
             }
-        digitalWrite(LATCH,1);
-
+        digitalWrite(LATCH,1);                  //Shift out
     }
 
 
@@ -126,89 +129,82 @@ void matrixDot(int x, int y)
 
 }
 
-void matrixArray(bool array[5][18],int t)
+void matrixArray(bool array[5][18],int t)       //Momentary function, that displays by an array[][] and a hold on time (t)
 {
-    for(int z(0);z<=t;z++)      //Hold on time
+    for(int z(0);z<=t;z++)                      //Hold on time
     {
-        for(int y(0); y <= 4; y++)      //Y row loop 
+        for(int y(0); y <= 4; y++)              //Change Y rows loop 
         {
             for(int i(0); i <= 4; i++) digitalWrite(yarray[i],0x01);    //Turn off all y pins (HIGH)
             for(int x(0); x<= 17; x++)                                  //X Column loop
             {
-                if(array[y][x] == true)     //only execute if pixel is on
+                if(array[y][x] == true)         //only execute if pixel is on
                 {
-                    switch(x)               //decide btw. pin 1, 2 o. shiftregister
+                    switch(x)                   //decide btw. pin 1, 2 o. shiftregister
                     {
-                        case 0: 
+                        case 0:                 //Pin A3 column 1
                                 digitalWrite(A3,0x01);
                                 digitalWrite(A2,0x00);
                                 digitalWrite(DATA,0x00);
                                 break;
-                        case 1: 
+                        case 1:                 //Pin A2 column 2
                                 digitalWrite(A2,0x01);
                                 digitalWrite(A3,0x00);
                                 digitalWrite(DATA,0x00);
                                 break;
-                        default:
+                        default:                //Registers columns 3+
                                 digitalWrite(DATA,0x01);
                                 digitalWrite(A3,0x00);
                                 digitalWrite(A2,0x00);
                     }
                 }
-                else
+                else                            //If no bit, than turn all pins off
                 {
                     digitalWrite(DATA,0x00);
                     digitalWrite(A2,0x00);
                     digitalWrite(A3,0x00);
                 }
-                CLOCKPULSE
+                CLOCKPULSE                      //Clockpulse afterevery cycle
             }
-            LATCHPULSE
-            digitalWrite(yarray[y],0x00);
-            delay(1);
+            LATCHPULSE                          //Latchpulse shift out 
+            digitalWrite(yarray[y],0x00);       //Set Y-pins columns to on (LOW)
+            delay(1);                           //Delay to display properly
         }
     }
 }
 
-void matrixClear()
+void matrixClear()                              //Display-clear function, hardwareside, no arrays
 {
-    digitalWrite(LATCH,0x00);
+    digitalWrite(LATCH,0x00);                   //Turn off Latch and data pins
     digitalWrite(DATA,0x00);
 
-    digitalWrite(A2,0x00);
+    digitalWrite(A2,0x00);                      //Also two X-pins
     digitalWrite(A3,0x00);
 
-    //for(int pin(4); pin <= 9; pin++) digitalWrite(pin,0x01);
-
-    for(int i(0); i <= 16; i++)
-    {
-        digitalWrite(CLOCK,0x01);
-        digitalWrite(CLOCK,0x00);
-    }
-    digitalWrite(LATCH,0x01);
-    digitalWrite(LATCH,0x00);
+    for(int i(0); i <= 16; i++) {CLOCKPULSE LATCHPULSE}     //Loop to pulse trough empty signal to registers
 }
 
-void matrixTest()
+void matrixTest()                               //Test function to check led's and buttons. No boundries
 {
-    for(int x(1), y(1); ; )
+    for(int x(1), y(1); ; )                     //Endless loop with (x) and (y) definition
     {
-		if(digitalRead(A0) == 1) y -= 1;
+		if(digitalRead(A0) == 1) y -= 1;        //Change position when pressin a button
 		else if(digitalRead(A1) == 1) x -= 1;
 		else if(digitalRead(A5) == 1) y += 1;
 		else if(digitalRead(A4) == 1) x += 1;
 
-		if(x == 19) x = 1;
+		if(x == 19) x = 1;                      //Jump to other side, when crossing boundry
 		else if( x== 0) x = 18;
 		if(y == 6)  y = 1;
 		else if(y == 0) y = 5;
-		matrixDot(x,y);
-		delay(100);
-		matrixClear();
+
+		matrixDot(x,y);                         //Display the dot, by function matrixDot(int x,int y);
+		delay(100);                             //Delay to reduce speed
+		matrixClear();                          //Clear screen, by function matrixClear();
     }
 }
 
-void squash()         //Pong-game knockoff
+void squash()                                   //Pong-game knockoff
 {
     int x(4),y(2),pb(2),cb(17),vx(0),vy(0),pp(2),cp(2),speed(20);       //Game variables
 
@@ -259,7 +255,7 @@ void squash()         //Pong-game knockoff
     }
 }
 
-void menu()         //Mainmenu loop
+void menu()                                     //Mainmenu loop
 {
     int men(0), mlimit(2);                                  //Limit of menuitems
     while(true)                                             //Endless loop
@@ -270,7 +266,7 @@ void menu()         //Mainmenu loop
 
         switch(men)                                         //switch for menuitems
         {
-            case 0:
+            case 0:                                         //Homescreen
                     matrixArray(hme,10);
                     break;
 
@@ -289,4 +285,4 @@ void menu()         //Mainmenu loop
     }
 }
 
-#endif
+#endif                                          //End of definition
